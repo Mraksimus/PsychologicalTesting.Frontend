037599f0f290a02be71e9173d968f1ec278791ae
masha <mary200438@gmail.com>
2025-10-27 12:46:12 +0300

    fix(PWPT-5): fixing HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import React from "react";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import {Welcome} from "./components/Welcome/Welcome";
 import { HomePage } from "@/pages/HomePage";
 
 export default function App() {
@@ -13,9 +12,10 @@ export default function App() {
             <Notifications position="top-right" />
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Navigate to="/login" replace />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/" element={<HomePage />} />
+                    <Route path="/register" element={<HomePage />} />
+                    <Route path="/welcome" element={<Welcome />} />
                 </Routes>
             </BrowserRouter>
         </MantineProvider>
