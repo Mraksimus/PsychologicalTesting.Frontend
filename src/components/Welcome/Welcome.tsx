d3cbd85c6426d62aa5e3e0cb2fab0c5d3d96f719
masha <mary200438@gmail.com>
2025-10-27 12:09:32 +0300

    feature(PWPT-5): Editing Welcome.tsx

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -5,10 +5,7 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Добро пожаловать на {' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          сервис психологического тестирования
-        </Text>
+        Добро пожаловать на сервис психологического тестирования
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         //тут будет текст//
