6ac2d2c47ab2154e2d6feee1b59c1a66735f9583
Mraksimus <maks.cucheryavenko@gmail.com>
2025-10-30 15:39:06 +0300

    fix: change text color

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -4,10 +4,10 @@ import classes from './Welcome.module.css';
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title className={classes.title} ta="center" mt={100} style={{ color: 'white' }}>
         Добро пожаловать на сервис психологического тестирования
       </Title>
-      <Text ta="center" size="lg" maw={580} mx="auto" mt="xl" style={{ color: 'black' }}>
+      <Text ta="center" size="lg" maw={580} mx="auto" mt="xl" style={{ color: 'white' }}>
         Пройдите психологические тесты и получите персонализированные рекомендации от нашего AI-помощника
       </Text>
     </>
